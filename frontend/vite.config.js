e8166926c88648599cd39383a596494930539594
naufalhakm <naufalhakim6b@gmail.com>
2025-10-30 16:04:13 +0700

    feat: production nginx setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'livecomerce.laidtechnology.tech',
+      '.laidtechnology.tech'
+    ]
   },
   define: {
     global: 'globalThis',
